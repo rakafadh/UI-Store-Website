0c14bb4fdb7e6f09b16f9a19674589fa36b2df10
rakafadh <maharaka@gmail.com>
2025-05-01 23:34:16 +0700

    fix method cors

--- FrontEnd/src/services/api.js
+++ FrontEnd/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'store-ui-backend-production.up.railway.app';
+const API_URL = 'https://store-ui-backend-production.up.railway.app';
 
 const api = axios.create({
   baseURL: API_URL,
