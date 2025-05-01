037998fba34a346f255f550cf0c7c486babc2f72
rakafadh <maharaka@gmail.com>
2025-05-01 23:16:35 +0700

    fix method cors

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 const corsOptions = {
@@ -21,7 +20,3 @@ app.use('/item', require('./src/routes/item.route'));
 app.use('/store', require('./src/routes/store.route'));
 app.use('/user', require('./src/routes/user.route'));
 app.use('/transaction', require('./src/routes/transaction.route'));
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
