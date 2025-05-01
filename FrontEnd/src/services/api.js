b185576de721e4e03ff265e4bdb4ac5fdfd245e2
rakafadh <maharaka@gmail.com>
2025-05-01 23:25:02 +0700

    fix method cors

--- FrontEnd/src/services/api.js
+++ FrontEnd/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://cs9-maharakafadhilah-backend.vercel.app/';
+const API_URL = 'https://cs9-maharakafadhilah-backend.vercel.app';
 
 const api = axios.create({
   baseURL: API_URL,
